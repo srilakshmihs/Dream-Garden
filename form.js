74018b3d9e7f9134b383928f810080eec6d287c0
srilakshmihs <71500117+srilakshmihs@users.noreply.github.com>
2020-12-30 22:54:23 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,24 @@
+function placechoice(place)
+{
+ switch(place)
+ {
+    case 1:
+        alert("Event venue will be provided the registerer and the company will only organise the event at the respective palce\n");
+        break;
+    case 2:
+        alert("Event venue will be provided by the company based on the event and will be in charge of the venue\n");
+        break;
+    case 3:
+        alert("Event venue will be decieded after the further discussion with the company\n");
+        break;
+    default:
+        alert("error");
+        break;
+ }
+}
+
+function submit()
+{
+alert("THANK YOU FOR REGISTERING .We will further contact you with the provided information");
+                     
+}   
